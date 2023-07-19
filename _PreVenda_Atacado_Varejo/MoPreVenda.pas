c3b30698e2891a5b92dc21a52692e922fc996a02
Jorge Pereira Júnior <jorgejunior.jpj2016@gmail.com>
2023-07-19 14:40:17 -0300

    ✨ feat: Adiciona metodo para mostrar um modal de inputs para impressao de etiquetas das lojas grupo Alexandre

--- _PreVenda_Atacado_Varejo/MoPreVenda.pas
+++ _PreVenda_Atacado_Varejo/MoPreVenda.pas
@@ -821,6 +821,7 @@ type
     function ChamaInputBoxTipoForracao(const ACaption, APrompt: string): string;
     function ChamaInputBoxEtiquetaZanquy(const ACaption,APrompt: string): integer;
     function ChamaInputBoxEtiquetaGrupoAquarela(const ACaption,APrompt: string): integer;
+    function ChamaInputBoxEtiquetaGrupoLojasAlexandre(const ACaption,APrompt: string): integer;
     function ChamaInputBoxEtiquetaCAMPOS(const ACaption,APrompt: string): integer;
     function ChamaInputBoxTurno(const ACaption, APrompt: string): Integer;
     procedure AuxiliarLanctoNFe(i: Integer);
@@ -23872,6 +23873,111 @@ begin
     end;
   end;
 end;
+
+
+function TFrmPrincipalPreVenda.ChamaInputBoxEtiquetaGrupoLojasAlexandre(const ACaption,
+  APrompt: string): integer;
+var
+  Form: TForm;
+  Prompt: TLabel;
+  cbxFornecedor: TComboBox;
+  DialogUnits: TPoint;
+  ButtonTop, ButtonWidth, ButtonHeight: Integer;
+  Value: integer;
+  i: Integer;
+  Buffer: array [0 .. 51] of Char;
+
+begin
+  result := -1;
+  Form := TForm.Create(Application);
+
+  with Form do
+  begin
+    try
+      Canvas.Font := Font;
+
+      for i := 0 to 25 do
+        Buffer[i] := Chr(i + Ord('A'));
+      for i := 0 to 25 do
+        Buffer[i + 26] := Chr(i + Ord('a'));
+
+      GetTextExtentPoint(Canvas.handle, Buffer, 52, TSize(DialogUnits));
+      DialogUnits.x := DialogUnits.x div 52;
+      BorderStyle := bsDialog;
+      caption := ACaption;
+      ClientWidth := MulDiv(380, DialogUnits.x, 4);
+      ClientHeight := MulDiv(63, DialogUnits.y, 8);
+      Position := poScreenCenter;
+      Prompt := TLabel.Create(Form);
+
+      with Prompt do
+      begin
+        Parent := Form;
+        AutoSize := True;
+        Left := MulDiv(8, DialogUnits.x, 4);
+        Top := MulDiv(8, DialogUnits.y, 8);
+        caption := APrompt;
+      end;
+
+      cbxFornecedor := TComboBox.Create(Form);
+
+      with cbxFornecedor do
+      begin
+        Parent := Form;
+        ItemIndex := 5;
+        Left := Prompt.Left;
+        Top := MulDiv(19, DialogUnits.y, 8);
+        width := MulDiv(340, DialogUnits.x, 4);
+        Style := csDropDownList;
+        // MaxLength := 255;
+        // aqui vc pode adicionar a data de como vai ser exibida no input:
+        // EditMask := '99/99/9999';
+        // PasswordChar := '*';
+        // SelectAll;
+        cbxFornecedor.Items.Add('EQUILIBRIO');
+      end;
+
+      ButtonTop := MulDiv(41, DialogUnits.y, 8);
+      ButtonWidth := MulDiv(50, DialogUnits.x, 4);
+      ButtonHeight := MulDiv(14, DialogUnits.y, 8);
+
+      with TButton.Create(Form) do
+      begin
+        Parent := Form;
+        caption := 'OK';
+        ModalResult := mrOK;
+        Default := True;
+        SetBounds(MulDiv(38, DialogUnits.x, 4), ButtonTop, ButtonWidth,
+          ButtonHeight);
+      end;
+
+      with TButton.Create(Form) do
+      begin
+        Parent := Form;
+        caption := 'Cancel';
+        ModalResult := mrCancel;
+        Cancel := True;
+        SetBounds(MulDiv(92, DialogUnits.x, 4), ButtonTop, ButtonWidth,
+          ButtonHeight);
+      end;
+
+      cbxFornecedor.ItemIndex := 0;
+
+      if ShowModal = mrOK then
+      begin
+        Value := cbxFornecedor.ItemIndex;
+        result := Value;
+      end;
+
+    finally
+
+      Form.Free;
+      Form := nil;
+
+    end;
+  end;
+end;
+
 
 function TFrmPrincipalPreVenda.ChamaInputBoxTurno(const ACaption,
   APrompt: string): Integer;
