9bf19d4dafc5fb88d3811195d8e8a8301865cdb0
Jorge Pereira Júnior <jorgejunior.jpj2016@gmail.com>
2023-07-27 11:36:51 -0300

    ✨ feat: Cria metodo de impressao de etiqueta gondola modelo g001

---
+++ _PreVenda_Atacado_Varejo/SistemaEtiquetas/tags/gondolas/g001/Prevenda.TagsGondola.G001.pas
@@ -0,0 +1,21 @@
+unit Prevenda.TagsGondola.G001;
+
+interface
+
+type
+  TGondola001 = class
+    private
+    public
+      procedure PrintTagGondolaG001;
+  end;
+
+implementation
+
+{ TGondola001 }
+
+procedure TGondola001.PrintTagGondolaG001;
+begin
+// CODE HERE
+end;
+
+end.
