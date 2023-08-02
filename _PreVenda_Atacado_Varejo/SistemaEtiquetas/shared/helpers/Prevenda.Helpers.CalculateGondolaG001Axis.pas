114fe858bf4accd44431f8c0e27802bc497de70d
Jorge Pereira Júnior <jorgejunior.jpj2016@gmail.com>
2023-08-02 09:57:37 -0300

    🚧 ✨ feat: Criando metodos para capturar e calcular os valores dos eixos X e Y que serao gravados no arquivo de etiqueta para impressao

--- _PreVenda_Atacado_Varejo/SistemaEtiquetas/shared/helpers/Prevenda.Helpers.CalculateGondolaG001Axis.pas
+++ _PreVenda_Atacado_Varejo/SistemaEtiquetas/shared/helpers/Prevenda.Helpers.CalculateGondolaG001Axis.pas
@@ -13,6 +13,19 @@ type
     public
       function GetG001DescriptionXValue: string;
       function GetG001DescriptionYValue: string;
+
+      function GetG001UnityXValue: string;
+      function GetG001UnityYValue: string;
+
+      function GetG001BarcodeSymbolXValue: string;
+      function GetG001BarcodeSymbolYValue: string;
+      function GetG001BarcodeValueXValue: string;
+      function GetG001BarcodeValueYValue: string;
+
+      function GetG001PriceSymbolXValue: string;
+      function GetG001PriceSymbolYValue: string;
+      function GetG001PriceValueXValue: string;
+      function GetG001PriceValueYValue: string;
   end;
 
 implementation
@@ -21,6 +34,7 @@ implementation
 
 uses
   Prevenda.Constants.GondolaG001;
+
 
 function TGondolaG001Calcs.GetG001DescriptionXValue: string;
 
@@ -46,7 +60,6 @@ begin
 end;
 
 
-
 function TGondolaG001Calcs.GetG001DescriptionYValue: string;
 
 var
@@ -68,6 +81,263 @@ begin
   end;
 
   result := (DESCRIPTION_Y_DEFAULT + ExternalOffset).ToString;
+end;
+
+
+
+
+function TGondolaG001Calcs.GetG001UnityXValue: string;
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetXFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (UNITY_X_DEFAULT + ExternalOffset).ToString;
+end;
+
+
+
+function TGondolaG001Calcs.GetG001UnityYValue: string;
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetYFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (UNITY_Y_DEFAULT + ExternalOffset).ToString;
+end;
+
+
+
+
+function TGondolaG001Calcs.GetG001BarcodeSymbolXValue: string;
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetXFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (BARCODE_SYMBOL_X_DEFAULT + ExternalOffset).ToString;
+end;
+
+
+
+function TGondolaG001Calcs.GetG001BarcodeSymbolYValue: string;
+
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetYFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (BARCODE_SYMBOL_Y_DEFAULT + ExternalOffset).ToString;
+end;
+
+
+
+
+
+function TGondolaG001Calcs.GetG001BarcodeValueXValue: string;
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetXFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (BARCODE_VALUE_X_DEFAULT + ExternalOffset).ToString;
+end;
+
+
+
+function TGondolaG001Calcs.GetG001BarcodeValueYValue: string;
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetYFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (BARCODE_VALUE_Y_DEFAULT + ExternalOffset).ToString;
+end;
+
+
+
+
+
+function TGondolaG001Calcs.GetG001PriceSymbolXValue: string;
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetXFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (PRICE_SYMBOL_X_DEFAULT + ExternalOffset).ToString;
+end;
+
+
+
+function TGondolaG001Calcs.GetG001PriceSymbolYValue: string;
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetYFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (PRICE_SYMBOL_Y_DEFAULT + ExternalOffset).ToString;
+end;
+
+
+
+
+
+function TGondolaG001Calcs.GetG001PriceValueXValue: string;
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetXFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (PRICE_VALUE_X_DEFAULT + ExternalOffset).ToString;
+end;
+
+function TGondolaG001Calcs.GetG001PriceValueYValue: string;
+
+var
+  Config: TConfigurationFileReader;
+
+  ExternalOffset: integer;
+
+begin
+  Config := TConfigurationFileReader.Create;
+
+  try
+
+    ExternalOffset := Config.ReadOffsetYFromConfigurationFile;
+
+  finally
+
+    Config.Free;
+
+  end;
+
+  result := (PRICE_VALUE_Y_DEFAULT + ExternalOffset).ToString;
 end;
 
 end.
