b750bb7b04d941f020ab741d13e450519dc76445
Jorge Pereira Júnior <jorgejunior.jpj2016@gmail.com>
2023-08-03 08:57:43 -0300

    🚧 ✨ feat: Criando metodos para capturar e calcular os valores dos eixos(descricao) X e Y que serao gravados no arquivo de etiqueta para impressao

--- _PreVenda_Atacado_Varejo/PreVenda.dpr
+++ _PreVenda_Atacado_Varejo/PreVenda.dpr
@@ -97,7 +97,9 @@ uses
   Prevenda.Helpers.CalculateGondolaG002DescriptionAxis in 'SistemaEtiquetas\shared\helpers\gondolas\g002\Prevenda.Helpers.CalculateGondolaG002DescriptionAxis.pas',
   Prevenda.Helpers.CalculateGondolaG002UnityAxis in 'SistemaEtiquetas\shared\helpers\gondolas\g002\Prevenda.Helpers.CalculateGondolaG002UnityAxis.pas',
   Prevenda.Helpers.CalculateGondolaG002PriceAxis in 'SistemaEtiquetas\shared\helpers\gondolas\g002\Prevenda.Helpers.CalculateGondolaG002PriceAxis.pas',
-  Prevenda.TagsGondola.G002 in 'SistemaEtiquetas\tags\gondolas\g002\Prevenda.TagsGondola.G002.pas';
+  Prevenda.TagsGondola.G002 in 'SistemaEtiquetas\tags\gondolas\g002\Prevenda.TagsGondola.G002.pas',
+  Prevenda.Constants.GondolaG003 in 'SistemaEtiquetas\shared\constants\Prevenda.Constants.GondolaG003.pas',
+  Prevenda.Helpers.CalculateGondolaG003DescriptionAxis in 'SistemaEtiquetas\shared\helpers\gondolas\g003\Prevenda.Helpers.CalculateGondolaG003DescriptionAxis.pas';
 
 {$R *.RES}
 var
