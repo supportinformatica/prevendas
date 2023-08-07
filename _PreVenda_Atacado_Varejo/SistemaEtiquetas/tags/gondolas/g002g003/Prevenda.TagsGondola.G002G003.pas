1ef198fd682247dd972ed448578c446e9bf3ccd6
Jorge Pereira Júnior <jorgejunior.jpj2016@gmail.com>
2023-08-07 08:39:56 -0300

    ♻️ 🚧 build: Implementando a juncao dos modelos G002 e G003

--- _PreVenda_Atacado_Varejo/SistemaEtiquetas/tags/gondolas/g002g003/Prevenda.TagsGondola.G002G003.pas
+++ _PreVenda_Atacado_Varejo/SistemaEtiquetas/tags/gondolas/g002g003/Prevenda.TagsGondola.G002G003.pas
@@ -13,11 +13,15 @@ uses
 
   Prevenda.TagsGondola.G002G003_Header,
   Prevenda.TagsGondola.G002G003_Body,
+  Prevenda.TagsGondola.G003_Body,
   Prevenda.TagsGondola.G002G003_Footer,
 
   Prevenda.Helpers.CalculateGondolaG002G003DescriptionAxis,
   Prevenda.Helpers.CalculateGondolaG002G003UnityAxis,
-  Prevenda.Helpers.CalculateGondolaG002G003PriceAxis;
+  Prevenda.Helpers.CalculateGondolaG002G003PriceAxis,
+
+  Prevenda.Helpers.CalculateGondolaG003PriceAtacadoAxis,
+  Prevenda.Helpers.CalculateGondolaG003PriceVarejoAxis;
 
 type
   TGondolaG002G003 = class
@@ -48,11 +52,17 @@ var
 
   Header: TGondolaG002G003Header;
   Body: TGondolaG002G003Body;
+  BodyAtacado: TGondolaG003Body;
   Footer: TGondolaG002G003Footer;
 
   G002G003Description: TGondolaG002G003DescriptionCalcs;
   G002G003Unit: TGondolaG002G003UnityCalcs;
   G002G003Price: TGondolaG002G003PriceCalcs;
+
+  G003PriceVarejo: TGondolaG003PriceVarejoCalcs;
+  G003PriceAtacado: TGondolaG003PriceAtacadoCalcs;
+
+  condicao: boolean;
 
 begin
 
@@ -76,11 +86,14 @@ begin
 
     Header := TGondolaG002G003Header.Create;
     Body   := TGondolaG002G003Body.Create;
+    BodyAtacado := TGondolaG003Body.Create;
     Footer := TGondolaG002G003Footer.Create;
 
     G002G003Description := TGondolaG002G003DescriptionCalcs.Create;
     G002G003Unit := TGondolaG002G003UnityCalcs.Create;
     G002G003Price := TGondolaG002G003PriceCalcs.Create;
+
+    condicao := true;
 
     try
       Header.Mount('I8,1,001', 'Q240,25', 'q832', 'D13', 'O', 'JF', 'ZB');
@@ -89,8 +102,17 @@ begin
 
       Body.MountUnity(G002G003Unit.GetG002G003UnityXValue, G002G003Unit.GetG002G003UnityYValue, Produto.unidade.unidade);
 
-      Body.MountPriceLabel(G002G003Price.GetG002G003PriceLabelXValue, G002G003Price.GetG002G003PriceLabelYValue, 'Pr. Varejo');
-      Body.MountPriceValue(G002G003Price.GetG002G003PriceValueXValue, G002G003Price.GetG002G003PriceValueYValue, Produto.vlPreco);
+
+      if (condicao = false) then begin
+        Body.MountPriceLabel(G002G003Price.GetG002G003PriceLabelXValue, G002G003Price.GetG002G003PriceLabelYValue, 'Pr. Varejo');
+        Body.MountPriceValue(G002G003Price.GetG002G003PriceValueXValue, G002G003Price.GetG002G003PriceValueYValue, Produto.vlPreco);
+      end else begin
+        Body.MountPriceLabel(G003PriceVarejo.GetG003PriceLabelXValue, G003PriceVarejo.GetG003PriceLabelYValue, 'Pr. Varejo');
+        Body.MountPriceValue(G003PriceVarejo.GetG003PriceValueXValue, G003PriceVarejo.GetG003PriceValueYValue, Produto.vlPreco);
+
+        Body.MountPriceLabel(G003PriceAtacado.GetG003PriceLabelXValue, G003PriceAtacado.GetG003PriceLabelYValue, 'Pr. Atacado');
+        Body.MountPriceValue(G003PriceAtacado.GetG003PriceValueXValue, G003PriceAtacado.GetG003PriceValueYValue, Produto.vlPreco);
+      end;
 
       Footer.Mount(FrmPrincipalPreVenda.SgDados.Cells[2, Line]);
 
@@ -100,6 +122,7 @@ begin
 
       Header.Free;
       Body.Free;
+      BodyAtacado.Free;
       Footer.Free;
 
       G002G003Description.Free;
